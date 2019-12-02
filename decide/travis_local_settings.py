08f1ed9a8e8a8188d5ee98726966a0e9bcd2e437
Antonio Sánchez Hipona <antonihipona@gmail.com>
2019-12-02 09:40:32 +0100

    Add db name and password to travis_local_settings.py

--- decide/travis_local_settings.py
+++ decide/travis_local_settings.py
@@ -30,8 +30,9 @@ BASEURL = 'http://localhost:8000'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decide',
+        'NAME': 'test-decide',
         'USER': 'decide',
+	'PASSWORD': 'decide',
         'HOST': 'localhost',
         'PORT': '5432',
     }
